bee0a4395f98aaee838e5bdf9648a4e6c4da47fa
anapaulab3rto <anapaulab3rto@gmail.com>
2025-03-27 22:13:56 -0300

    create fucao plantar semente

--- src/jogar.pl
+++ src/jogar.pl
@@ -1,14 +1,16 @@
+% File: jogar.pl
 :- module(jogar, [iniciarJogo/0]).
-:- use_module(tabuleiro, [iniciarTabuleiros/3, exibirTabuleiros/3, plantar/5]).
+:- use_module(tabuleiro, [iniciarTabuleiros/3, exibirTabuleiros/3]).
+:- use_module(controllerPlantas, [plantarSemente/9]).  % Import the 9-arity version
 
 iniciarJogo :-
-    /* Função temporária para iniciar o jogo, atualmente só inicia o tabuleiro e o exibe
-    */
     iniciarTabuleiros(Passado, Presente, Futuro),
     exibirTabuleiros(Passado, Presente, Futuro),
-    writeln('\nPlantando na posição (2,3) do tabuleiro do passado:'),
-    plantar(Passado, '🌱', 2, 3, TabuleiroModificado),
-    exibirTabuleiros(TabuleiroModificado, Presente, Futuro).
+    writeln('\nPlantando na posição (2,3) do tabuleiro do presente:'),
+    plantarSemente(Passado, Presente, Futuro, futuro, 2, 3, 
+                NovoPassado, NovoPresente, NovoFuturo),
+
+    exibirTabuleiros(NovoPassado, NovoPresente, NovoFuturo).
 
 
 
