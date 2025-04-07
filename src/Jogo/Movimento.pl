6722ef893db3bb7eb611538d173c0b11126f2325
brunaletsleticia <140714125+brunaletsleticia@users.noreply.github.com>
2025-04-07 00:56:16 -0300

    documentado movimento

--- src/Jogo/Movimento.pl
+++ src/Jogo/Movimento.pl
@@ -4,14 +4,23 @@
 :- use_module('./src/Interface/Jogador.pl').
 
 movimento(Tabuleiro, Passado, Presente, Futuro, Foco, Linha, Coluna, NovaLinha, NovaColuna, PecaEsperada, NovoPassado, NovoPresente, NovoFuturo) :-
-    /* Efetua o movimento do jogador com as coordenadas já escolhidas.
-    Args: 
-        Linha, Coluna: posição atual da peça.
-        NovaLinha, NovaColuna: posição para onde mover.
-        PecaEsperada: Peça do jogador. 
-        TabuleiroAtualizado: Novo tabuleiro após a movimentação.
-    */
-    
+/* Efetua o movimento do jogador com as coordenadas já escolhidas, verificando e removendo sementes se necessário
+
+ * @param Tabuleiro Tabuleiro antes do movimento.
+ * @param Passado Tabuleiro do passado.
+ * @param Presente Tabuleiro do presente.
+ * @param Futuro Tabuleiro do futuro.
+ * @param Foco Tempo atual do jogador (passado, presente ou futuro).
+ * @param Linha Linha atual da peça a ser movida.
+ * @param Coluna Coluna atual da peça a ser movida.
+ * @param NovaLinha Linha de destino do movimento.
+ * @param NovaColuna Coluna de destino do movimento.
+ * @param PecaEsperada Peça esperada na posição de destino para validação do movimento.
+ * @param NovoPassado Tabuleiro do passado atualizado após o movimento.
+ * @param NovoPresente Tabuleiro do presente atualizado após o movimento.
+ * @param NovoFuturo Tabuleiro do futuro atualizado após o movimento.
+ */
+
     semente(Semente),
     nth1(NovaLinha, Tabuleiro, LinhaDestinoLista),
     nth1(NovaColuna, LinhaDestinoLista, ConteudoDestino),
@@ -32,4 +41,3 @@ movimento(Tabuleiro, Passado, Presente, Futuro, Foco, Linha, Coluna, NovaLinha, 
     ; Foco == futuro -> (NovoPassado = TempPassado, NovoPresente = TempPresente, NovoFuturo = TabuleiroAtualizado)
     ).
     
-
